d5ad04b1ae37a1631803743517de7cc6fbce7b92
Sameed Akber <sameedakber.ai@gmail.co>
2024-04-10 15:37:07 -0400

    Setup django unicorn

--- DocumentSearch/urls.py
+++ DocumentSearch/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from pages.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page, name="home")
+    path('', home_page, name="home"),
+    path("unicorn/", include("django_unicorn.urls")),
 ]
