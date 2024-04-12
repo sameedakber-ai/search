4fdf76a0603635f609ca379fcf2974feb4d20c26
Sameed Akber <sameedakber.ai@gmail.co>
2024-04-12 17:01:53 -0400

    Create fetch api for document upload

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from pages.forms import DirectoryForm
+from pages.forms import DirectoryForm, Directory
 # Create your views here.
 
 def home_page(request, *args, **kwargs):
-    if request.method == 'POST':
-        form = DirectoryForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-    form = DirectoryForm()
-    context = {'form': form}
-    return render(request, "pages/home.html", context=context)
+    if request.method == 'POST' and request.FILES['file']:
+        Directory.objects.create(directory=request.FILES['file'])
+        print(request.FILES['file'])
+    return render(request, "pages/home.html")
