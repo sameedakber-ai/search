bb7782cdec3e62a46bb456e0ea3f1c1ece04d726
Sameed Akber <sameedakber.ai@gmail.co>
2024-04-12 17:22:11 -0400

    Update fetch api to handle multiple file uploads

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,8 @@ from pages.forms import DirectoryForm, Directory
 # Create your views here.
 
 def home_page(request, *args, **kwargs):
-    if request.method == 'POST' and request.FILES['file']:
-        Directory.objects.create(directory=request.FILES['file'])
-        print(request.FILES['file'])
+    if request.method == 'POST':
+        for i in range(len(request.FILES)):
+            file = request.FILES['file_{}'.format(i)]
+            Directory.objects.create(directory=file)
     return render(request, "pages/home.html")
