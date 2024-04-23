1d814c05301b2dd0356bc07036f78339e2444d0c
Sameed Akber <sameedakber.ai@gmail.co>
2024-04-23 18:05:34 -0400

    Update settings for deployment

--- DocumentSearch/settings.py
+++ DocumentSearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@ew1un2++#uidqjt3y0@+p8=++!2cah8fhlcvyl4c4j1&m0$+t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -101,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS = ["https://doc-search-cd06661b1976.herokuapp.com", 'http://127.0.0.1:8000']
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -115,10 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
