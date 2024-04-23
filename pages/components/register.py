0f2c6cc59098ae1c6c1d873e50901bc9584f5f07
Sameed Akber <sameedakber.ai@gmail.co>
2024-04-23 10:40:15 -0400

    Fix login/register authentication

--- pages/components/register.py
+++ pages/components/register.py
@@ -1,17 +1,39 @@
-from django_unicorn.components import UnicornView,LocationUpdate
+from django_unicorn.components import UnicornView, LocationUpdate
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
+from django.contrib import messages
 
 
 class RegisterView(UnicornView):
     username = ''
     email = ''
     password = ''
+    authenticated = True
 
     def register(self):
-        user = User.objects.filter(username=self.username)
-        if not user:
+
+        self.authenticated = True
+
+        user = User.objects.filter(email=self.email)
+
+        if len(self.username) <= 4:
+            messages.error(self.request, "Username: minimum 5 characters")
+            self.authenticated = False
+
+        if len(self.password) <= 6:
+            messages.error(self.request, "Password: minimum 6 characters")
+            self.authenticated = False
+
+        if User.objects.filter(username=self.username):
+            messages.error(self.request, "Username: username is taken")
+            self.authenticated = False
+
+        elif user:
+            messages.error(self.request, "Email: user already exists")
+            self.authenticated = False
+
+        if self.authenticated:
             user = User.objects.create_user(username=self.username, email=self.email, password=self.password)
             login(self.request, user)
             return redirect('/')
