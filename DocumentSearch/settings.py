18c8c0fecf58d801e976204053d1b373821a0b72
Sameed Akber <sameedakber.ai@gmail.co>
2024-04-25 10:19:17 -0400

    Set whitenoise to serve static files in the cloud

--- DocumentSearch/settings.py
+++ DocumentSearch/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "django_unicorn",
     'pages',
-    'storages',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +122,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
