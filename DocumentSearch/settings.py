91eed53f1e0c4f120eda95c949a5cc1fb2168978
Sameed Akber <sameedakber.ai@gmail.co>
2024-04-25 19:42:08 -0400

    Increase the maximum number of uploaded files

--- DocumentSearch/settings.py
+++ DocumentSearch/settings.py
@@ -131,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 MEDIA_ROOT = "media"
+
+DATA_UPLOAD_MAX_NUMBER_FILES = 50000
